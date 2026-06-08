ccd35ace289fb2c11df1d0d4f9c95cace1a28bcb
Bucket-yeah <75127235+Bucket-yeah@users.noreply.github.com>
2026-06-08 17:13:55 +0500

    Add files via upload

---
+++ obj/Debug/net472/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace RavenM
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "RavenM";
+        public const string PLUGIN_NAME = "RavenM";
+        public const string PLUGIN_VERSION = "0.7";
+    }
+}
